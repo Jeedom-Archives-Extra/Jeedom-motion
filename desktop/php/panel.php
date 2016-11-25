22ee20a938399cfa27d3406ea4497f2fb432574d
Halbout Michael <michaelhalbout@gmail.com>
2016-11-25 08:37:16 +0100

    Update panel.php

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -13,34 +13,32 @@ if (!is_object($object)) {
 $child_object = object::buildTree($object);
 $parentNumber = array();
 ?>
-
 <div class="row row-overflow">
-    <?php
-if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
-	echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
-} else {
-	echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
-}
-?>
-
-    <div class="bs-sidebar">
-        <ul id="ul_object" class="nav nav-list bs-sidenav">
-            <li class="nav-header">{{Liste objets}} </li>
-            <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-            <?php
-$allObject = object::buildTree(null, true);
-foreach ($allObject as $object_li) {
-	$parentNumber[$object_li->getId()] = $object_li->parentNumber();
-	$margin = 15 * $parentNumber[$object_li->getId()];
-	if ($object_li->getId() == $object->getId()) {
-		echo '<li class="cursor li_object active" ><a href="index.php?v=d&p=panel&m=motion&object_id=' . $object_li->getId() . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
-	} else {
-		echo '<li class="cursor li_object" ><a href="index.php?v=d&p=panel&m=motion&object_id=' . $object_li->getId() . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
-	}
-}
-?>
-        </ul>
-    </div>
+    	<?php
+		if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
+			echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
+		} else {
+			echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
+		}
+	?>
+    	<div class="bs-sidebar">
+        	<ul id="ul_object" class="nav nav-list bs-sidenav">
+            		<li class="nav-header">{{Liste objets}} </li>
+            		<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+			<?php
+				$allObject = object::buildTree(null, true);
+				foreach ($allObject as $object_li) {
+					$parentNumber[$object_li->getId()] = $object_li->parentNumber();
+					$margin = 15 * $parentNumber[$object_li->getId()];
+					if ($object_li->getId() == $object->getId()) {
+						echo '<li class="cursor li_object active" ><a href="index.php?v=d&p=panel&m=motion&object_id=' . $object_li->getId() . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
+					} else {
+						echo '<li class="cursor li_object" ><a href="index.php?v=d&p=panel&m=motion&object_id=' . $object_li->getId() . '" style="position:relative;left:' . $margin . 'px;">' . $object_li->getHumanName(true) . '</a></li>';
+					}
+				}
+			?>
+        	</ul>
+   	</div>
 </div>
 <?php
 if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
@@ -57,7 +55,7 @@ echo '<div class="div_displayEquipement" style="width: 100%;">';
 if (init('object_id') == '') {
 	foreach ($allObject as $object) {
 		foreach ($object->getEqLogic(true, false, 'motion') as $camera) {
-			echo $camera->toHtml('dview');
+			echo $camera->toHtml();
 		}
 	}
 } else {
@@ -68,13 +66,10 @@ if (init('object_id') == '') {
 		$cameras = $child->getEqLogic(true, false, 'motion');
 		if (count($cameras) > 0) {
 			foreach ($cameras as $camera) {
-				echo $camera->toHtml('dview');
+				echo $camera->toHtml();
 			}
 		}
 	}
 }
 echo '</div>';
-?>
-</div>
-</div>
-<?php include_file('desktop', 'panel', 'js', 'motion');?>
+include_file('desktop', 'panel', 'js', 'motion');?>
