8e10bd42202826d5c0e89c8066541fb8bee39127
Halbout Michael <michaelhalbout@gmail.com>
2016-11-30 12:17:49 +0100

    Update motion.js

--- desktop/js/motion.js
+++ desktop/js/motion.js
@@ -115,10 +115,7 @@ $('body').on('click','.editMaphilightArea', function() {
 	if($(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=maphilightArea]').val()!= undefined)
 		areas=$(this).closest('.cmd').find('.cmdAttr[data-l1key=configuration][data-l2key=maphilightArea]').val().split(',');
 	
-	$('#md_modal').dialog({
-		title: "{{Editer la zone de detection}}",
-		height: 600,
-		width: 550});
+	$('#md_modal').dialog({title: "{{Editer la zone de detection}}"});
 	$('#md_modal').load('index.php?v=d&plugin=motion&modal=maphilight.motion&id=' + $('.eqLogicAttr[data-l1key=id]').val());
 	$('#md_modal').dialog('open');
 	$("#md_modal").dialog('option', 'buttons', {
