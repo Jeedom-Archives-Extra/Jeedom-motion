d75125e29b7e3e6d1c5664921467576aa56b423d
Halbout Michael <michaelhalbout@gmail.com>
2016-11-30 08:25:57 +0100

    Update motion.js

--- desktop/js/motion.js
+++ desktop/js/motion.js
@@ -140,7 +140,7 @@ $('body').on('click','.editArea', function() {
 		title: "{{Editer la zone de detection}}",
 		height: 600,
 		width: 550});
-	$('#md_modal').load('index.php?v=d&plugin=motion&modal=area.motion')
+	$('#md_modal').load('index.php?v=d&plugin=motion&modal=area.motion&id='. $('.eqLogicAttr[data-l1key=id]').val());
 	$('#md_modal').dialog('open');
 	$("#md_modal").dialog('option', 'buttons', {
 		"Annuler": function() {
