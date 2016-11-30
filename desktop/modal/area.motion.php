83913980c7d0324cbe8df2f9102619b0974ed65a
Halbout Michael <michaelhalbout@gmail.com>
2016-11-30 08:22:37 +0100

    Mise a jours de la strategie

--- desktop/modal/area.motion.php
+++ desktop/modal/area.motion.php
@@ -2,7 +2,8 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
- ?>
+$eqLogic=eqLogic::byId(init('id'));
+?>
 <style>
 .AreaContent
 {
@@ -37,11 +38,31 @@ if (!isConnect('admin')) {
 	opacity: 0.5
 }
 </style>
-<div class="AreaContent"></div>
+<div class="AreaContent">
+	<img src="<?php echo $eqLogic->getSnapshot();?>"/>
+	<span class="Areas"></span>
+</div>
 <div id="debug"></div>
 <script>
-var eqLogiqId=$('.eqLogicAttr[data-l1key=id]').val();
- $('body').on('click','.Area',function (e) {
+$('.AreaContent').find('img').load(function() {
+	for(var loop=0; loop<9; loop++){
+		$('.AreaContent').find('.Areas')
+			.append($('<div>')
+				.addClass('Area')
+				.attr('id','area_'+loop));
+		if(areas.indexOf(loop+1)>=0)
+			$('.AreaContent').find('#area_'+loop).addClass('Select');
+	};
+	var offsetImg = $('.AreaContent').find('img').offset();
+	var offsetArea =$('.AreaContent').find('.Areas').offset();
+	$('.AreaContent').find('.Areas').css('width', $(this).width());
+	$('.AreaContent').find('.Areas').css('height',$(this).height());
+	$('.AreaContent').find('.Area').css('width', $(this).width()/3);
+	$('.AreaContent').find('.Area').css('height',$(this).height()/3);
+	$('.AreaContent').find('.Areas').css('left',offsetImg.left - offsetArea.left);
+	$('.AreaContent').find('.Areas').css('top', offsetImg.top - offsetArea.top);
+});
+$('body').on('click','.Area',function (e) {
 	var AreaSelect=parseInt($(this).attr('id').split('_')[1])+1;
 	if (areas.indexOf(AreaSelect)>=0)
 	{
@@ -54,50 +75,4 @@ var eqLogiqId=$('.eqLogicAttr[data-l1key=id]').val();
 		areas=areas+AreaSelect;
 	}
 }); 
-$.ajax({
-	type: 'POST',
-	url: 'plugins/motion/core/ajax/motion.ajax.php',
-	data: {
-		action: 'WidgetHtml',
-		cameraId:eqLogiqId
-	},
-	dataType: 'json',
-	global: false,
-	error: function (request, status, error) {
-		handleAjaxError(request, status, error, $('#div_updatepreRequisAlert'));
-	},
-	success: function (data) {
-		if (data.result)
-		{
-			$('.AreaContent').append(data.result);
-			$('.AreaContent').find('.ImgVideoFlux'+eqLogiqId).clone().appendTo(".AreaContent");
-			$('.AreaContent').find('.eqLogic').remove('.eqLogic');
-			
-			$('.AreaContent').find('.ImgVideoFlux'+eqLogiqId).load(function() {
-				//alert('width: '+$(this).width()+' height:'+$(this).height());
-				if ($('.AreaContent').find('.Areas').length==0){
-					$('.AreaContent').append($('<center>').append($('<span>').addClass('Areas')));
-					for(var loop=0; loop<9; loop++)
-					{
-						$('.AreaContent').find('.Areas')
-							.append($('<div>')
-								.addClass('Area')
-								.attr('id','area_'+loop));
-						if(areas.indexOf(loop+1)>=0)
-							$('.AreaContent').find('#area_'+loop).addClass('Select');
-					};
-				}
-				var offsetImg = $('.AreaContent').find('.ImgVideoFlux'+eqLogiqId).offset();
-				var offsetArea =$('.AreaContent').find('.Areas').offset();
-				$('.AreaContent').find('.Areas').css('width', $(this).width());
-				$('.AreaContent').find('.Areas').css('height',$(this).height());
-				$('.AreaContent').find('.Area').css('width', $(this).width()/3);
-				$('.AreaContent').find('.Area').css('height',$(this).height()/3);
-				$('.AreaContent').find('.Areas').css('left',offsetImg.left - offsetArea.left);
-				$('.AreaContent').find('.Areas').css('top', offsetImg.top - offsetArea.top);
-				
-			});
-		}
-	}
-});
 </script>
