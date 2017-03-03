165f7fb975d5f5931bccf590c1b7089ff2cfd83a
Halbout Michael <michaelhalbout@gmail.com>
2017-03-03 08:24:09 +0100

    Update motion.displayImage.php

--- desktop/modal/motion.displayImage.php
+++ desktop/modal/motion.displayImage.php
@@ -2,19 +2,5 @@
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-//echo init('src');
-if(strpos(init('src'),'.jpg')>0){
-	echo '<center><img class="img-responsive" src="' . init('src') . '" /></center>';
-else
-	?>
-<center>
-	<video width="320" height="240" autoplay>
-	  	<source src="' . init('src') . '" type="video/mp4">
-	  	<source src="' . init('src') . '" type="video/ogg">
-	  	<source src="' . init('src') . '" type="video/flv">
-		Your browser does not support the video tag.
-	</video>
-</center>
-<?php
-}
+echo '<center><img class="img-responsive" src="' . init('src') . '" /></center>';
 ?>
