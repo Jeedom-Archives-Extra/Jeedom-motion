65c5c393a1245bd258d2ffdca32b5f100e73efe6
Halbout Michael <michaelhalbout@gmail.com>
2017-03-17 11:55:03 +0100

    Update stream.php

--- core/php/stream.php
+++ core/php/stream.php
@@ -1,23 +1,22 @@
 <?php
 set_time_limit(120);
- 
-$port = $_GET['port']; 
- 
-$src = fopen(urldecode($_REQUEST['url'], 'rb'));
+$url=urldecode($_REQUEST['url']);
 $timeout = time();
- 
-header('Max-Age: 0');
-header('Expires: 0');
-header('Cache-Control: no-cache, private');
-header('Pragma: no-cache');
-#header('Content-Type: video/mpeg'); 
-header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
- 
-while ( !feof($src) ) 
-{
-	echo  fread($src, 2048);
-	if ( (time() - $timeout) >= 120 ) {
-		exit();
-	}
+if(!$src=@fopen($url,"rb")){
+    log::add('motion','debug','Impossible d\'ouvrir le flux video '.$url);
+	echo 'plugins/motion/core/template/icones/no-image-blanc.png';
+}else {
+  header('Max-Age: 0');
+  header('Expires: 0');
+  header('Cache-Control: no-cache, private');
+  header('Pragma: no-cache');
+  #header('Content-Type: video/mpeg'); 
+  header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
+  while ( !feof($src) ) {
+    echo  fread($src, 2048);
+    if ( (time() - $timeout) >= 120 ) {
+    	exit();
+    }
+  }
 }
 ?>
