79ba67216894d93fd491b29138f8a71bedff35ea
Halbout Michael <michaelhalbout@gmail.com>
2017-03-17 11:00:01 +0100

    Create stream.php

---
+++ core/php/stream.php
@@ -0,0 +1,23 @@
+<?php
+set_time_limit(120);
+ 
+$port = $_GET['port']; 
+ 
+$src = fopen($_REQUEST['url'], 'rb');
+$timeout = time();
+ 
+header('Max-Age: 0');
+header('Expires: 0');
+header('Cache-Control: no-cache, private');
+header('Pragma: no-cache');
+#header('Content-Type: video/mpeg'); 
+header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
+ 
+while ( !feof($src) ) 
+{
+	echo  fread($src, 2048);
+	if ( (time() - $timeout) >= 120 ) {
+		exit();
+	}
+}
+?>
