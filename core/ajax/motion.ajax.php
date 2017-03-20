02348d1ba9450adb03601875c474c9b295add7d8
Halbout Michael <michaelhalbout@gmail.com>
2017-03-20 14:09:24 +0100

    Update motion.ajax.php

--- core/ajax/motion.ajax.php
+++ core/ajax/motion.ajax.php
@@ -84,62 +84,21 @@
 			exec('sudo rm /etc/motion/motion.log > /dev/null 2>/dev/null &');
 			ajax::success("Suppression faite");
 		}
-		if (init('action') == 'getVideoConvertionStat') {
-			$dir=dirname(__FILE__) . '/../../../../tmp/';
-			$result = array();
-			$result['duration'] = 0;
-			$result['time'] = 0;
-			$result['progress'] = 0;
-			$result['video']=$dir.'video.mp4';
-			$result['videoType']="video/mp4";
-			if(file_exists($result['video']))
-				exec('sudo rm '.$result['video']);
-			$LogFile=$dir.'convert.txt';
-			if(!file_exists($LogFile)){
-				$cmd = 'sudo ffmpeg -i '.init('src').' -vcodec libx264 -vpre lossless_slow -crf 25 -acodec libfaac -threads 0 -t 60 '.$result['video'];
-				//$cmd = 'sudo ffmpeg -i  -vcodec libx264 '.$result['video'];
-				$cmd .= ' >> ' . $LogFile . ' 2>&1 &';
-				exec($cmd);
-			}
-			//exec('sudo ffmpeg -i '.init('src').' -vcodec libtheora '.$dir.'video.ogv');
-			//exec('sudo ffmpeg -i '.init('src').'  -b 1000k '.$dir.'video.webm');
-			if(!$log=@fopen($LogFile,"r")){
-				ajax::error('Impossible d\'ouvrir le fichier : '.$LogFile);
-			}
-			else {
-				$content=fgets($log);
-				preg_match('/Duration: (.*?), start:/',$content,$matches) ;
-				if(count($matches)>0 ){
-					$rawDuration = $matches[1];
-					// convert rawDuration from 00:00:00.00 to seconds.
-					$ar = array_reverse(explode(":",$rawDuration));
-					$result['duration'] = floatval($ar[0]);
-					if ($ar[1]) $result['duration'] += intval($ar[1]) * 60;
-					if ($ar[2]) $result['duration'] += intval($ar[2]) * 60 * 60;
-					
-					// get the time 
-					
-					preg_match('/time=(.*?) bitrate/g',$content,$matches) ;
-					if(count($matches)>0 ){
-						$rawTime = array_pop($matches);
-						$rawTime = str_replace('time=','',$rawTime);
-						$rawTime = str_replace(' bitrate','',$rawTime);
-						
-						// convert rawTime from 00:00:00.00 to seconds.
-						$ar = array_reverse(explode(":",$rawTime));
-						$result['time'] = floatval($ar[0]);
-						if ($ar[1]) $result['time'] += intval($ar[1]) * 60;
-						if ($ar[2]) $result['time'] += intval($ar[2]) * 60 * 60;
-						
-						//calculate the progress
-						$result['progress'] = round(($result['time']/$result['duration']) * 100);
-					}
-					if($result['progress'] == 100)
-						exec('sudo rm '.$LogFile);
+		if (init('action') == 'getCoord') {
+			$return='';
+			$cmd=cmd::byId(init('action'));
+			if(is_object($cmd)){
+				switch($cmd->getLogicalId()){
+					case 'detect':	
+						$return= $cmd->getConfiguration('DetectArea')
+					break;
+					case 'maphilight':
+						$return= $cmd->getConfiguration('maphilightArea');
+					break;
 				}
-				ajax::success($result);
 			}
-		}	
+			ajax::success($return);
+		}
 		throw new Exception(__('Aucune methode correspondante à : ', __FILE__) . init('action'));
 		/*     * *********Catch exeption*************** */
 	} catch (Exception $e) {
