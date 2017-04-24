40018e782fee74a414f466be9b7e27c29028402a
Halbout Michael <michaelhalbout@gmail.com>
2017-04-24 14:23:38 +0200

    Update stream.php

--- core/php/stream.php
+++ core/php/stream.php
@@ -5,22 +5,19 @@ $url=urldecode($_REQUEST['url']);
 $timeout = time();
 if(!$src=@fopen($url,"rb")){
 	log::add('motion','debug','Impossible d\'ouvrir le flux video '.$url);
-	$path = 'plugins/motion/core/template/icones/no-image-blanc.png';
-	$type = pathinfo($path, PATHINFO_EXTENSION);
-	$data = file_get_contents($path);
-	echo 'data:image/' . $type . ';base64,' . base64_encode($data);
-}else {
-  header('Max-Age: 0');
-  header('Expires: 0');
-  header('Cache-Control: no-cache, private');
-  header('Pragma: no-cache');
-  #header('Content-Type: video/mpeg'); 
-  header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
-  while ( !feof($src) ) {
-    echo  fread($src, 2048);
-    if ( (time() - $timeout) >= 120 ) {
-    	exit();
-    }
-  }
+	$src=@fopen('plugins/motion/core/template/icones/no-image-blanc.png',"rb")
 }
+header('Max-Age: 0');
+header('Expires: 0');
+header('Cache-Control: no-cache, private');
+header('Pragma: no-cache');
+#header('Content-Type: video/mpeg'); 
+header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
+while ( !feof($src) ) {
+	echo  fread($src, 2048);
+	if ( (time() - $timeout) >= 120 ) {
+		exit();
+	}
+}
+
 ?>
