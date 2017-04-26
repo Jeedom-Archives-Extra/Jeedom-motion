d878d362357f36d4c13af1c59e6e9e7152cd7a82
Halbout Michael <michaelhalbout@gmail.com>
2017-04-26 09:26:53 +0200

    Update stream.php

--- core/php/stream.php
+++ core/php/stream.php
@@ -5,7 +5,7 @@ $url=urldecode($_REQUEST['url']);
 $timeout = time();
 if(!$src=@fopen($url,"rb")){
 	log::add('motion','debug','Impossible d\'ouvrir le flux video '.$url);
-	$src=@fopen('plugins/motion/core/template/icones/no-image-blanc.png',"rb");
+	//$src=@fopen('plugins/motion/core/template/icones/no-image-blanc.png',"rb");
 }else{
 header('Max-Age: 0');
 header('Expires: 0');
@@ -13,12 +13,12 @@ header('Cache-Control: no-cache, private');
 header('Pragma: no-cache');
 #header('Content-Type: video/mpeg'); 
 header('Content-Type: multipart/x-mixed-replace; boundary=BoundaryString');
-}
+
 while ( !feof($src) ) {
 	echo  fread($src, 2048);
 	if ( (time() - $timeout) >= 120 ) {
 		exit();
 	}
 }
-
+}
 ?>
