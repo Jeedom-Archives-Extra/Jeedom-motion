fcb87c62697fb9c99b4c1debacae2268c365fc56
Halbout Michael <michaelhalbout@gmail.com>
2017-05-23 13:32:54 +0200

    Update install.sh

--- ressources/install.sh
+++ ressources/install.sh
@@ -12,29 +12,35 @@ then
 	#rm -R /etc/motion/
 fi
 echo "*****************************************************************************************************"
+echo "*                                   Installation des dépendance                                     *"
+echo "*****************************************************************************************************"
+sudo apt-get install -y --force-yes autoconf
+sudo apt-get install -y --force-yes automake
+sudo apt-get install -y --force-yes pkgconf
+sudo apt-get install -y --force-yes libtool
+sudo apt-get install -y --force-yes libjpeg8-dev
+sudo apt-get install -y --force-yes build-essential
+sudo apt-get install -y --force-yes libzip-dev
+echo "*****************************************************************************************************"
 echo "*                                          Installation de FFMPEG                                   *"
 echo "*****************************************************************************************************"
-test=$(grep '#http://www.deb-multimedia.org' /etc/apt/sources.list)
-if [ -z "$test" ] || [ $test = " " ] || [ $test = "" ]
-then 
-	echo "#http://www.deb-multimedia.org" | sudo tee -a /etc/apt/sources.list
-	echo "deb http://www.deb-multimedia.org jessie main non-free" | sudo tee -a /etc/apt/sources.list
-	echo "deb-src http://www.deb-multimedia.org jessie main non-free" | sudo tee -a /etc/apt/sources.list
-fi 
-echo 30 > /tmp/compilation_motion_in_progress
-sudo apt-get install -y --force-yes  deb-multimedia-keyring
-echo 40 > /tmp/compilation_motion_in_progress
-sudo apt-get -y update
-echo 50 > /tmp/compilation_motion_in_progress
-sudo apt-get install -y --force-yes ffmpeg
-sudo apt-get install -y --force-yes v4l-utils
-echo 60 > /tmp/compilation_motion_in_progress
-sudo apt-get install -y --force-yes x264
-echo 70 > /tmp/compilation_motion_in_progress
+git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
+cd ffmpeg
+./configure --prefix=/etc/ffmpeg
+make -j3
+sudo make install
+cd ../motion/
+PKG_CONFIG_PATH=/etc/ffmpeg/lib/pkgconfig cmake .
+make
 echo "*****************************************************************************************************"
 echo "*                                          Compilation de motion:                                   *"
 echo "*****************************************************************************************************"
-sudo apt-get install -y --force-yes motion
+git clone https://github.com/Motion-Project/motion.git motion
+cd motion
+sudo autoreconf -fiv
+./configure --prefix=/etc/motion --with-ffmpeg=/etc/ffmpeg
+make
+sudo make install
 echo 90 > /tmp/compilation_motion_in_progress
 sudo chmod -R 777 /etc/motion/
 echo 95 > /tmp/compilation_motion_in_progress
