b4f7f538f8591353183a57314d10c156f946d8f4
Halbout Michael <michaelhalbout@gmail.com>
2017-07-10 09:28:43 +0200

    Ajout d'un script de suppression des dependance

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,4 +5,12 @@ function motion_update() {
         $motion->save();
     }
 }
+function motion_remove() {
+    exec('sudo /etc/init.d/motion stop');
+    exec('sudo rm -rf /usr/local/src/motion/');
+    exec('sudo rm -rf /etc/motion/');
+    exec('sudo groupdel motion');
+    exec('sudo rm /etc/init.d/motion');
+    exec('sudo rm /etc/rc*.d/*motion');
+}
 ?>
