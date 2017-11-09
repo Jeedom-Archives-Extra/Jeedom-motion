55675896aa9dc070eaf3d0e453b36ad5ee68fe93
Halbout Michael <michaelhalbout@gmail.com>
2017-11-09 08:56:43 +0100

    Update motion.php

--- desktop/php/motion.php
+++ desktop/php/motion.php
@@ -495,7 +495,8 @@ $eqLogics = eqLogic::byType('motion');
 					<div class="form-group">
 						<label class="col-md-5 control-label">{{Fichier PGM à utiliser comme un masque de sensibilité.}}</label>
 						<div class="col-md-7">
-							<input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mask_file" />
+							<input type="file"/>
+							<input type="file" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="mask_file"  data-url="plugins/motion/core/ajax/motion.ajax.php?action=addFileMask" />
 						</div>
 					</div>
 					<div class="form-group">
